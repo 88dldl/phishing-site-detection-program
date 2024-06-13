9607aa1e3374e0a85dbe99d475d3f7541d3e0060
88dldl <chltnrgml0999@gmail.com>
2024-06-13 19:11:25 +0900

    feat : 비표준 포트 열림 확인 추가

--- src/phishing_detection/url_features_extractor.py
+++ src/phishing_detection/url_features_extractor.py
@@ -2,6 +2,7 @@ import whois
 
 from src.phishing_detection.checker.Ip_address_checker import check_ip_adderss
 from src.phishing_detection.checker.date_checker import check_creation_date, check_expiration_date
+from src.phishing_detection.checker.port_open_checker import check_non_standard_ports
 from src.phishing_detection.checker.url_checker import check_https_token, check_at_symbol, check_url_length
 
 from src.phishing_detection.checker.redirection_count_checker import check_redirection_count
@@ -62,5 +63,9 @@ def extract_url_features(url):
     # 10. 리다이렉션 횟수
     redirection_count = check_redirection_count(url)
     features.append(redirection_count)
+
+    #11. 비표준 포트 열림 확인
+    non_standard = check_non_standard_ports(url)
+    features.append(non_standard)
 
     return features
